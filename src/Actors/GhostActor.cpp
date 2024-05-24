085d3bc2325e2d56841e0eba67a13ded8fc401f1
Artem GL <dlya-igor-artem@yandex.ru>
2024-05-24 18:31:24 +0300

    fixedMovment

--- src/Actors/GhostActor.cpp
+++ src/Actors/GhostActor.cpp
@@ -55,56 +55,74 @@ bool Ghost::CheckBaned2() {
 void Ghost::GhostGetTarget(Position pacman, std::vector<bool> collisions) {
 
 	short tempDir = _direction;
-	
-	if (!collisions[0] && _door_cord.y != this->get_position().y + _speed && get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x, pacman.y + _speed) && (tempDir != (2 + 0) % 4) && (pacman.y + _speed != _door_cord.y)) {
-		_direction = 0;
-
-	}
-	else if (!collisions[2] && _door == 1 && (_door_cord.y != this->get_position().y + _speed * TILE_SIZE) && get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x, pacman.y - _speed) && (tempDir != (2 + 2) % 4) && pacman.y - _speed != _door_cord.y) {
-		_direction = 2;
-
-	}
-	else if (!collisions[3] && _door_cord.x != this->get_position().x + _speed && CheckBaned2() && get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x + _speed, pacman.y) && (tempDir != (2 + 3) % 4) && pacman.x + _speed != _door_cord.x) {
-		_direction = 3;
-
-	}
-	else if (!collisions[1] && (_door_cord.x != this->get_position().x - _speed) && /*CheckBaned1()*//*this->get_position().x - _speed * TILE_SIZE != _Banned1.x && this->get_position().x - _speed * TILE_SIZE != _Banned2.x*/ CheckBaned1() && get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x - _speed, pacman.y) && (tempDir != (2 + 1) % 4)&& pacman.x - _speed != _door_cord.x) {
-
-		_direction = 1;
-	}
-	
-	
-	
-	else if(tempDir == _direction && collisions[_direction]) {
-		for (int i = 0; i < 4; i++) {
+	short avalibale_ways=0;
+	float Min_dist = sqrt(pow((MAP_HEIGHT * TILE_SIZE), 2) + pow((MAP_WIDTH * TILE_SIZE), 2));
+	float Dist[4] = { get_dist_targ(pacman.x, pacman.y + _speed), get_dist_targ(pacman.x - _speed, pacman.y), get_dist_targ(pacman.x, pacman.y - _speed), get_dist_targ(pacman.x + _speed, pacman.y) };
+	for (short i = 0; i < 4; i++) {
+		if (!collisions[i]) {
+			avalibale_ways++;
 			if (!collisions[i] && (tempDir != (2 + i) % 4)) {
-				_direction = i;
+				if (Dist[i] < Min_dist) {
+					Min_dist = Dist[i];
+				}
+			}
+			else {
+				Dist[i]= sqrt(pow((MAP_HEIGHT * TILE_SIZE), 2) + pow((MAP_WIDTH * TILE_SIZE), 2));
 			}
 		}
 	}
+	if (avalibale_ways > 1) {
+		//float Min_dist = sqrt(pow((MAP_HEIGHT * TILE_SIZE), 2) + pow((MAP_WIDTH * TILE_SIZE), 2));
+		if (!collisions[0] && _door_cord.y != this->get_position().y + _speed && Dist[0] == Min_dist /*get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x, pacman.y + _speed)*/ && (tempDir != (2 + 0) % 4) && (pacman.y + _speed != _door_cord.y)) {
+			_direction = 0;
 
-	if (_direction != -1 && !collisions[_direction]) {
-		switch (_direction) {
-		case 0:
-		{
-			_position.y -= _speed;
-			break;
 		}
-		case 1:
-		{
-			_position.x += _speed;
-			break;
+		else if (!collisions[2] && _door == 1 && (_door_cord.y != this->get_position().y + _speed * TILE_SIZE) && Dist[2] == Min_dist/*get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x, pacman.y - _speed)*/ && (tempDir != (2 + 2) % 4) && pacman.y - _speed != _door_cord.y) {
+			_direction = 2;
+
 		}
-		case 2:
-		{
-			_position.y += _speed;
-			break;
+		else if (!collisions[3] && _door_cord.x != this->get_position().x + _speed && CheckBaned2() && Dist[3] == Min_dist /*get_dist_targ(pacman.x, pacman.y) > get_dist_targ(pacman.x + _speed, pacman.y)*/ && (tempDir != (2 + 3) % 4) && pacman.x + _speed != _door_cord.x) {
+			_direction = 3;
+
 		}
-		case 3:
-		{
-			_position.x -= _speed;
-			break;
+		else if (!collisions[1] && (_door_cord.x != this->get_position().x - _speed) &&  CheckBaned1() && Dist[1] == Min_dist && (tempDir != (2 + 1) % 4) && pacman.x - _speed != _door_cord.x) {
+
+			_direction = 1;
 		}
+
+
+
+		else if (tempDir == _direction && collisions[_direction]) {
+			for (int i = 0; i < 4; i++) {
+				if (!collisions[i] && (tempDir != (2 + i) % 4)) {
+					_direction = i;
+				}
+			}
+		}
+
+		if (_direction != -1 && !collisions[_direction]) {
+			switch (_direction) {
+			case 0:
+			{
+				_position.y -= _speed;
+				break;
+			}
+			case 1:
+			{
+				_position.x += _speed;
+				break;
+			}
+			case 2:
+			{
+				_position.y += _speed;
+				break;
+			}
+			case 3:
+			{
+				_position.x -= _speed;
+				break;
+			}
+			}
 		}
 	}
 }
